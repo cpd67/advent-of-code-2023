3971ebc53c6cd5276682ccd380ccaa3d71ac9389
Chris Dilley <codemonkey941@gmail.com>
2023-12-02 21:33:17 -0500

    Tweak quotes

--- day-1/part-1/calibration.py
+++ day-1/part-1/calibration.py
@@ -1,8 +1,8 @@
 import re
 
-if __name__ == "__main__":
-    calibration_doc = open("../calibration-document.txt")
-    digit_regex = re.compile("[0-9]")
+if __name__ == '__main__':
+    calibration_doc = open('../calibration-document.txt')
+    digit_regex = re.compile('[0-9]')
     calibration_sum = 0
     for line in calibration_doc:
         digits = digit_regex.findall(line)
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         last_digit = digits[-1]
         calibration_value = int(first_digit + last_digit)
         calibration_sum += calibration_value
-    print(f"Done! Calibration sum: {calibration_sum}")
+    print(f'Done! Calibration sum: {calibration_sum}')
--- day-1/part-2/improved-calibration.py
+++ day-1/part-2/improved-calibration.py
@@ -1,18 +1,12 @@
 import re
 
-if __name__ == "__main__":
-    calibration_doc = open("../calibration-document.txt")
-    digit_regex = re.compile("(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))")
-    name_to_digit_lookup = {
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': "9",
+if __name__ == '__main__':
+    calibration_doc = open('../calibration-document.txt')
+    # Note that we may have overlapping matches with this regex, so we use
+    # a lookahead to deal with them
+    digit_regex = re.compile('(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))')
+    name_to_digit_lookup = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+                            'six': '6', 'seven': '7', 'eight': '8', 'nine': '9',
     }
     calibration_sum = 0
     for line in calibration_doc:
@@ -27,6 +21,8 @@ if __name__ == "__main__":
             first_digit = name_to_digit_lookup[first_digit]
         if not last_digit.isdigit():
             last_digit = name_to_digit_lookup[last_digit]
+        print(first_digit, last_digit)
         calibration_value = int(first_digit + last_digit)
+        print(calibration_value)
         calibration_sum += calibration_value
-    print(f"Done! Calibration sum: {calibration_sum}")
+    print(f'Done! Calibration sum: {calibration_sum}')
