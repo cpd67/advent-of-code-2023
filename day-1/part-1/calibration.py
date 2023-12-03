3262cd13ac7b0040f4d60ab023f25e85b8bd5b63
Chris Dilley <codemonkey941@gmail.com>
2023-12-02 21:10:42 -0500

    Improve part 1 submission

--- day-1/part-1/calibration.py
+++ day-1/part-1/calibration.py
@@ -2,15 +2,14 @@ import re
 
 if __name__ == "__main__":
     calibration_doc = open("../calibration-document.txt")
-    digit_regex = re.compile("\d+")
+    digit_regex = re.compile("[0-9]")
     calibration_sum = 0
     for line in calibration_doc:
         digits = digit_regex.findall(line)
         # findall() gives us a list of matches, which will be a list of 
-        # digit strings. We want the first digit of the first match,
-        # and the last digit of the last match.
-        first_digit = digits[0][0]
-        last_digit = digits[-1][-1]
+        # digit strings. We want the first and last matches.
+        first_digit = digits[0]
+        last_digit = digits[-1]
         calibration_value = int(first_digit + last_digit)
         calibration_sum += calibration_value
     print(f"Done! Calibration sum: {calibration_sum}")
