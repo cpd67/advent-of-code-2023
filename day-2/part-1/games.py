bd3eb6c7a550d7ee047cd37a2fc10103c8f0140e
Chris Dilley <codemonkey941@gmail.com>
2023-12-05 20:11:13 -0500

    Day 2, part 1 done

--- day-2/part-1/games.py
+++ day-2/part-1/games.py
@@ -1,9 +1,29 @@
+max_cube_values = {'red': 12, 'green': 13, 'blue': 14}
+
+class InvalidCubeConfiguration(Exception):
+    pass
+
+
+def check_cube_constraint(cube_str):
+    """
+    Check that a cube value is at or below the max value for that cube.
+    """
+    cube_value, cube_name = cube_str.split(' ')
+    if int(cube_value) > max_cube_values[cube_name]:
+        raise InvalidCubeConfiguration()
+
 
 if __name__ == "__main__":
     with open("../list-of-games.txt") as games:
-        max_red = 12
-        max_green = 13
-        max_blue = 14
+        id_sum = 0
         for game in games:
-            if game != "":
-                print(game)
+            game_id, cube_subsets = game.split(":")
+            try:
+                for cube_subset in cube_subsets.split(';'):
+                    for cube in cube_subset.split(','):
+                        check_cube_constraint(cube.strip())
+                # All games are good, this is one of the IDs to sum
+                id_sum += int(game_id.split(' ')[1])
+            except InvalidCubeConfiguration:
+                pass
+        print(f"Done! The sum is: {id_sum}")
